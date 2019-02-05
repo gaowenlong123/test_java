b096432136dd58c579c8b3da3233f584f72b6251
gaowenlong123 <1044073802@qq.com>
2019-02-05 10:50:02 +0800

    add

---
+++ test_web/src/com/gao/web/servlet/test_Cookie.java
@@ -0,0 +1,12 @@
+package com.gao.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Administrator on 2019/2/5 0005.
+ */
+
+@WebServlet("/test_Cookie")
+public class test_Cookie extends HttpServlet {
+}
