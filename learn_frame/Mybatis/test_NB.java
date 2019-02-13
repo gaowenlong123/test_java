9c0574115cf29c8a49700323638992673d53dd89
gaowenlong123 <1044073802@qq.com>
2019-02-13 13:22:30 +0800

    add

---
+++ learn_frame/Mybatis/test_NB.java
@@ -0,0 +1,17 @@
+package Mybatis;
+
+/**
+ *
+ * 1： 逆向工程 ： 通过数据库中的单表 ，自动生成java代码 。很常用
+ *   生成简单的增，删，改，查
+ *
+ *    0：创建一个简单的java项目
+ *   1：导入jar包 ， 创建generator配置文件
+ *   2：使用java类来执行逆向工程
+ *   3：把生成的代码拷贝到项目中
+ *   4：在正式项目中使用逆向工程生成的代码
+ *
+ * Created by Administrator on 2019/2/13 0013.
+ */
+public class test_NB {
+}
